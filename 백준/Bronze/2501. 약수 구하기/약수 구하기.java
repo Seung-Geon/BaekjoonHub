a7c5fb6dd97790799130078d19b40768aba9e1ef
Seunggeon <sglee.dev@gmail.com>
2026-02-25 20:05:21 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,37 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            int count = 0;
+            int answer = 0;
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    count++;
+                }
+
+                if(count == k) {
+                    answer = i;
+                    break;
+                }
+            }
+
+            System.out.println(answer);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
